1b471950944afff0c944b5566240065e77176845
Remi Jorge <remijorge5@gmail.com>
2024-01-06 19:16:23 +0100

    add post ui

--- src/main/java/com/polytech/SocialBucket/UI/Post/AddPostController.java
+++ src/main/java/com/polytech/SocialBucket/UI/Post/AddPostController.java
@@ -1,133 +1,266 @@
 package com.polytech.SocialBucket.UI.Post;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
+import java.lang.Thread;
 
 import com.polytech.SocialBucket.Logic.User;
+import com.polytech.SocialBucket.Logic.Post;
 import com.polytech.SocialBucket.Logic.Facade.PostFacade;
 import com.polytech.SocialBucket.Logic.Facade.UserFacade;
 import com.polytech.SocialBucket.UI.FXRouter;
+import com.polytech.SocialBucket.UI.Post.PostComponent;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.application.Platform;
+
+import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+import java.io.File;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 
 public class AddPostController {
 
- @FXML
- private GridPane gridPane;
+    @FXML
+    private Label usernameLabel;
 
- @FXML
- private TextField textField;
+    @FXML
+    private Label followingLabel;
 
- @FXML
- private Label statusLabel;
+    @FXML
+    private Label followersLabel;
 
- @FXML
- private Label fileLabel;
+    @FXML
+    private ScrollPane mainContent;
 
- @FXML
- private ImageView imageView;
+    @FXML
+    private Pane navbar;
 
- private UserFacade userFacade = UserFacade.getInstance();
- private PostFacade postFacade = PostFacade.getInstance();
+    @FXML
+    private Button openButton;
 
- private File selectedFile;
- private Image selectedImage;
+    @FXML
+    private Button closeButton;
 
- @FXML
- private void initialize() {
-  // Initialize your UI components or add event handlers if needed
-  // For example, you can set default values, styles, or add listeners here
- }
+    @FXML
+    private TextField descriptionField;
 
- @FXML
- private void goToProfile() {
-  try {
-   FXRouter.goTo("profile");
-  } catch (Exception e) {
-   e.printStackTrace();
-  }
- }
+    @FXML
+    private Button imageButton;
 
- @FXML
- private void addFile() {
-  FileChooser fileChooser = new FileChooser();
-  fileChooser.setTitle("Select a file");
-  selectedFile = fileChooser.showOpenDialog(new Stage());
-  if (selectedFile != null) {
-   // Do something with the selected file
-   imageView.setImage(null);
-   selectedImage = null;
-   fileLabel.setText("File selected: " + selectedFile.getName());
-  } else {
-   fileLabel.setText("File selection canceled.");
-  }
- }
+    @FXML
+    private Button fileButton;
 
- @FXML
- private void addImage() {
-  FileChooser fileChooser = new FileChooser();
-  fileChooser.setTitle("Select an Image");
-  fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
-  selectedFile = fileChooser.showOpenDialog(new Stage());
-  if (selectedFile != null) {
-   // Load the selected image into the ImageView
-   selectedImage = new Image(selectedFile.toURI().toString());
-   imageView.setImage(selectedImage);
-   fileLabel.setText("Image selected: " + selectedFile.getName());
-  } else {
-   fileLabel.setText("Image selection canceled.");
-  }
- }
+    @FXML
+    private Button removeButton;
 
- @FXML
- private void handleCreatePost() throws IOException {
-  // This method is called when the login button is clicked
+    @FXML
+    private Label statusLabel;
 
-  // Get the entered username and password
-  String text = textField.getText();
-  User user = userFacade.getCurrentUser();
-  // Call the login method in UserFacade to validate the user
+    @FXML
+    private ImageView imageContainer;
 
-  // Check if the login was successful
-  if (user != null) {
-   // If successful, update the status label
-   // statusLabel.setText("Successfully logged in!");
-   // goToProfile();
-   if (text == null || text.isEmpty()) {
-    statusLabel.setText("please enter text");
-    return;
-   }
-   if (selectedImage == null && selectedFile == null) {
-    postFacade.createPost(text, "texte", null, user);
-    statusLabel.setText("Post created!");
-    return;
-   } else if (selectedImage != null && selectedFile != null) {
-    postFacade.createPost(text, "image", selectedFile, user);
-    statusLabel.setText("Post created!");
-    return;
-   } else if (selectedImage == null && selectedFile != null) {
-    postFacade.createPost(text, "file", selectedFile, user);
-    statusLabel.setText("Post created!");
-    return;
-   } else {
-    statusLabel.setText("Error while creating post, try again!");
-    return;
-   }
-  } else {
-   // If failed, update the status label
-   statusLabel.setText("Login failed, try again!");
-  }
+    @FXML
+    private Pane imageBox;
 
- }
+    @FXML
+    private Pane fileBox;
 
+
+    @FXML
+    private Label filenameLabel;
+
+
+    private UserFacade userFacade = UserFacade.getInstance();
+    private PostFacade postFacade = PostFacade.getInstance();
+
+    private Image selectedImage;
+    private File selectedFile;
+
+    @FXML
+    private void initialize() {
+        // init name
+        User user = UserFacade.getInstance().getCurrentUser();
+        if (user == null) {
+            usernameLabel.setText("Anonymous");
+            return;
+        }
+        String name = user.getUsername();
+        System.out.println(name);
+        usernameLabel.setText(name);
+        int nbFollowing = user.getNbFollowing();
+        int nbFollowers = user.getNbFollowers();
+        followingLabel.setText("Following: " + nbFollowing);
+        followersLabel.setText("Followers: " + nbFollowers);
+
+        removeButton.setVisible(false);
+        removeButton.setManaged(false);
+
+        handleButton(false);
+
+        openNavbar();
+    }
+
+    @FXML
+    private void goToProfile() {
+        try {
+            FXRouter.goTo("profile");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void openNavbar() {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/com/polytech/SocialBucket/sidebarPage.fxml"));
+            Pane navbarContent = loader.load();
+
+            navbar.setPrefWidth(140);
+            mainContent.setPrefWidth(705);
+
+            navbar.getChildren().add(navbarContent);
+
+            handleButtonNavbar(true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    private void closeNavbar() {
+        navbar.setPrefWidth(0);
+        mainContent.setPrefWidth(900);
+
+        handleButtonNavbar(false);
+
+        navbar.getChildren().clear();
+    }
+
+    private void handleButtonNavbar(boolean open) {
+        openButton.setVisible(!open);
+        openButton.setManaged(!open);
+        closeButton.setVisible(open);
+        closeButton.setManaged(open);
+        if (open) {
+            closeButton.toFront();
+        }
+    }
+
+    @FXML
+    private void handleCreatePost() {
+        String description = descriptionField.getText();
+        User user = UserFacade.getInstance().getCurrentUser();
+
+        if (user == null) {
+            statusLabel.setText("You must be logged in to post");
+            return;
+        }
+
+        if (description == null || description.isEmpty()) {
+            statusLabel.setText("Description cannot be empty");
+            return;
+        }
+        try {
+            if (selectedImage == null && selectedFile == null) {
+                postFacade.createPost(description, "texte", null, user);
+            } else if (selectedImage != null && selectedFile != null) {
+                postFacade.createPost(description, "image", selectedFile, user);
+            } else if (selectedImage == null && selectedFile != null) {
+                postFacade.createPost(description, "file", selectedFile, user);
+                statusLabel.setText("Post created!");
+            }
+            statusLabel.setText("Post created!");
+
+            Thread.sleep(1000);
+
+            goToProfile();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            statusLabel.setText("Error while creating post");
+        }
+
+    }
+
+    @FXML
+    private void addImage(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select an Image");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
+        selectedFile = fileChooser.showOpenDialog(new Stage());
+        if (selectedFile != null) {
+            selectedImage = new Image(selectedFile.toURI().toString());
+            imageContainer.setImage(selectedImage);
+            imageBox.setVisible(true);
+            int width = (int) selectedImage.getWidth();
+            int height = (int) selectedImage.getHeight();
+            if (width > height && width > 0) {
+                imageBox.setPrefHeight(height * 400 / width);
+            } else {
+                imageBox.setPrefHeight(400);
+            }
+            filenameLabel.setText("");
+            fileBox.setVisible(false);
+            fileBox.setPrefHeight(0);
+            handleButton(true);
+        } else {
+            filenameLabel.setText("");
+        }
+    }
+
+    private void handleButton(boolean hasFile) {
+        imageButton.setVisible(!hasFile);
+        imageButton.setManaged(!hasFile);
+        fileButton.setVisible(!hasFile);
+        fileButton.setManaged(!hasFile);
+        removeButton.setVisible(hasFile);
+        removeButton.setManaged(hasFile);
+    }
+
+    @FXML
+    private void addFile(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a file");
+        selectedFile = fileChooser.showOpenDialog(new Stage());
+        if (selectedFile != null) {
+            imageContainer.setImage(null);
+            selectedImage = null;
+            filenameLabel.setText(selectedFile.getName());
+            fileBox.setVisible(true);
+            fileBox.setPrefHeight(50);
+            imageBox.setVisible(false);
+            imageBox.setPrefHeight(0);
+            handleButton(true);
+        } else {
+            filenameLabel.setText("File selection canceled.");
+        }
+    }
+
+
+    @FXML
+    private void removeImageOrFile(){
+        selectedImage = null;
+        selectedFile = null;
+        imageContainer.setImage(null);
+        filenameLabel.setText("");
+        imageBox.setVisible(false);
+        imageBox.setPrefHeight(0);
+        fileBox.setVisible(false);
+        fileBox.setPrefHeight(0);
+        handleButton(false);
+    }
 }
